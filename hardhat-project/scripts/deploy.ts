9513500ab2d99149bb282549a34e751d8ca963e8
josealonso <jose.alonso.programmer@gmail.com>
2022-06-04 16:51:00 +0200

    Contract deployed and verified on Mumbai

--- hardhat-project/scripts/deploy.ts
+++ hardhat-project/scripts/deploy.ts
@@ -3,28 +3,63 @@
 //
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
-import { ethers } from "hardhat";
+import { HardhatUserConfig, internalTask, task } from "hardhat/config";
+// import { hardhatArgum } from "hardhat";
+import hre, { ethers } from "hardhat";
+import { FEE, VRF_COORDINATOR, LINK_TOKEN, KEY_HASH } from "../constants";
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
+  /*
+ A ContractFactory in ethers.js is an abstraction used to deploy new smart contracts,
+ so randomWinnerGame here is a factory for instances of our RandomWinnerGame contract.
+ */
+  const randomWinnerGame = await ethers.getContractFactory("RandomWinnerGame");
+  // deploy the contract
+  const deployedRandomWinnerGameContract = await randomWinnerGame.deploy(
+    VRF_COORDINATOR,
+    LINK_TOKEN,
+    KEY_HASH,
+    FEE
+  );
 
-  // We get the contract to deploy
-  const Greeter = await ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  await deployedRandomWinnerGameContract.deployed();
 
-  await greeter.deployed();
+  // print the address of the deployed contract
+  console.log(
+    "Verify Contract Address:",
+    deployedRandomWinnerGameContract.address
+  );
 
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("Sleeping.....");
+  // Wait for etherscan to notice that the contract has been deployed
+  await sleep(30000);
+
+  // Verify the contract after deploying
+  await hre.run("verify:verify", {
+    address: deployedRandomWinnerGameContract.address,
+    constructorArguments: [VRF_COORDINATOR, LINK_TOKEN, KEY_HASH, FEE],
+  });
+
+  // await hre.run("verify:verify", {
+  // task("verify", "verify the contract")
+  //   .addParam("address", deployedRandomWinnerGameContract.address)
+  //   .addParam("constructorArguments", "[VRF_COORDINATOR, LINK_TOKEN, KEY_HASH, FEE]")
+  //   .setAction(verify());
+  // // await task(
+  //   "verify:verify", deployedRandomWinnerGameContract.address,
+  //   "[VRF_COORDINATOR, LINK_TOKEN, KEY_HASH, FEE]",
+  // );
+
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+function sleep(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+// Call the main function and catch if there is any error
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
